d41be6425ec072b49c679fac1061c287774a0eb7
Ed <67418738+FreeHK-Lunity@users.noreply.github.com>
2023-09-28 00:50:09 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from colorama import  Back, Style
+
+print(Back.GREEN + 'The text with Green background')
+print(Style.DIM + 'The text is DIM now')
+
+Here is a list of values that you can try:
+
+Fore: BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, RESET.
+Back: BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, RESET.
+Style: DIM, NORMAL, BRIGHT, RESET_ALL
